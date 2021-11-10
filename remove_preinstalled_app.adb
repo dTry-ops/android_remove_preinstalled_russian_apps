399873ec8e532a4aeb247c7358f6e8ecf721b719
dtry <dtry@mac.local>
2021-11-10 20:23:18 +0700

    Initial commit

---
+++ remove_preinstalled_app.adb
@@ -0,0 +1,63 @@
+# Новости Mail.ru
+# https://play.google.com/store/apps/details?id=ru.mail.mailnews
+pm uninstall -k --user 0 ru.mail.mailnews
+
+# Почта Mail.ru
+# https://play.google.com/store/apps/details?id=ru.mail.mailapp
+pm uninstall -k --user 0 ru.mail.mailapp
+
+# Маруся - голосовой помощник
+# https://play.google.com/store/apps/details?id=ru.mail.search.electroscope
+pm uninstall -k --user 0 ru.mail.search.electroscope
+
+# ICQ мессенджер
+# https://play.google.com/store/apps/details?id=com.icq.mobile.client&hl=ru&gl=US
+pm uninstall -k --user 0 com.icq.mobile.client
+
+# ВКонтакте 
+# https://play.google.com/store/apps/details?id=com.vkontakte.android&hl=ru&gl=US
+pm uninstall -k --user 0 com.vkontakte.android
+
+# Одноклассники: социальная сеть
+# https://play.google.com/store/apps/details?id=ru.ok.android&hl=ru&gl=US
+pm uninstall -k --user 0 ru.ok.android
+
+# Ok Live - трасляции онлайн
+# https://play.google.com/store/apps/details?id=ru.ok.live&hl=ru&gl=US
+pm uninstall -k --user 0 ru.ok.live
+
+# Яндекс.Диск
+# https://play.google.com/store/apps/details?id=ru.yandex.disk&hl=ru&gl=US
+pm uninstall -k --user 0 ru.yandex.disk
+
+# Яндекс.Карты
+# https://play.google.com/store/apps/details?id=ru.yandex.yandexmaps&hl=ru&gl=US
+pm uninstall -k --user 0 ru.yandex.yandexmaps
+
+# Яндекс - с Алисой
+# https://play.google.com/store/apps/details?id=ru.yandex.searchplugin&hl=ru&gl=US
+pm uninstall -k --user 0 ru.yandex.searchplugin
+
+# Яндекс.Браузер
+# https://play.google.com/store/apps/details?id=com.yandex.browser&hl=ru&gl=US
+pm uninstall -k --user 0 com.yandex.browser
+
+# Платежная система МИР
+# https://play.google.com/store/apps/details?id=ru.nspk.mirpay&hl=ru&gl=US
+pm uninstall -k --user 0 ru.nspk.mirpay
+
+# Госуслуги
+# https://play.google.com/store/apps/details?id=ru.rostel&hl=ru&gl=US
+pm uninstall -k --user 0 ru.rostel
+
+# Программа по установке российского ПО
+# https://play.google.com/store/apps/details?id=com.minsvyaz.applist&hl=ru&gl=US
+pm uninstall -k --user 0 com.minsvyaz.applist
+
+# Kaspersky антивирус
+# https://play.google.com/store/apps/details?id=com.kms.free&hl=ru&gl=US
+pm uninstall -k --user 0 com.kms.free
+
+# МойОфис Документы
+# https://play.google.com/store/apps/details?id=com.ncloudtech.cloudoffice&hl=ru&gl=US
+pm uninstall -k --user 0 com.ncloudtech.cloudoffice
